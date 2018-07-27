f6a7e331aa38b72b3906bc52784f7a5c02f7a507
Mateusz Ezechiel Górka <admin@yehezkel.cba.pl>
2018-07-28 01:59:08 +0200

    bugfixes, refactoring
    - more exact conversion (no unnecessary new line sings)
    - applyRules completly refactored (last one was fucking hell)
    - bold, italics, striking were working only when preceded  by whitespaces, fixed

--- vsmd.js
+++ vsmd.js
@@ -1,34 +1,47 @@
 function markdown(code) {
+  const inln = "";    /* inline rule */
+  const whln = "\n";  /* whole line rule*/
+
+  /*
+    p0  group which will always be printed
+    p1  ACTUAL TEXT
+    p2  a/img src/href
+    p3  img title
+  */
+
   const makeTag =
-    (tag, value) => typeof value == "string"
-      ? "<" + tag + ">" + value + "</" + tag + ">"
-      : "<" + tag + " />";
+    (tag, mode, p0, value, p2, p3) => p0 + (tag == "img_with_title"
+      ? "<img alt=\"" + value + "\" src=\"" + p2 + "\" title=\"" + p3 + "\" />"
+      : tag == "img_without_title"
+        ? "<img alt=\"" + value + "\" src=\"" + p2 + "\" />"
+        : tag == "a"
+          ? "<a href=\"" + p2 + "\">" + value + "</a>"
+          : typeof value == "string"
+            ? "<" + tag + ">" + value + "</" + tag + ">"
+            : "<" + tag + " />") + mode;
 
   const applyRules = 
     (str, arr) =>
-      [...Array(arr.length).keys()]
-      .map(x => ["".replace, arr[x][0], arr[x][1]])
-      .reduce((prev, cur) =>
-        cur[0].call(prev,                    /* str.replace(rule, tag) */
-                    new RegExp(cur[1], "img"), 
-                    (a, b) => makeTag(cur[2], b)), str);
+      arr.reduce((prev, cur) =>
+        prev.replace(new RegExp(cur[0], "g"), /* str.replace(rule, tag) */
+                     (a, p0, p1, p2, p3) => makeTag(cur[1], cur[2], p0, p1, p2, p3)), str);
   
   code = "\n" + code;
 
   return applyRules(code, [
-    ["\n[ \t]*######[ \t]*(.*?)\n", "h6"], 
-    ["\n[ \t]*#####[ \t]*(.*?)\n", "h5"],
-    ["\n[ \t]*####[ \t]*(.*?)\n", "h4"],
-    ["\n[ \t]*###[ \t]*(.*?)\n", "h3"],
-    ["\n[ \t]*##[ \t]*(.*?)\n", "h2"],
-    ["\n[ \t]*#[ \t]*(.*?)\n", "h1"],
-    ["\n\\*{3,}\n", "hr"],
-    ["\n_{3,}\n", "hr"],
-    ["[^\\\\]\\*\\*(.*?[^\\\\])\\*\\*", "b"],
-    ["[^\\\\]\\*(.*?[^\\\\])\\*", "i"],
-    ["[^\\\\]__(.*?[^\\\\])__", "b"],
-    ["[^\\\\]_(.*?[^\\\\])_", "i"],
-    ["[^\\\\]~~(.*?[^\\\\])~~", "s"],
-    ["\n(.*?)\n=+?\n", "h1"],
-    ["\n(.*?)\n-+?\n", "h2"]]);
+    ["(\n)[ \t]*######[ \t]*(.*?)\n", "h6", whln], 
+    ["(\n)[ \t]*#####[ \t]*(.*?)\n", "h5", whln],
+    ["(\n)[ \t]*####[ \t]*(.*?)\n", "h4", whln],
+    ["(\n)[ \t]*###[ \t]*(.*?)\n", "h3", whln],
+    ["(\n)[ \t]*##[ \t]*(.*?)\n", "h2", whln],
+    ["(\n)[ \t]*#[ \t]*(.*?)\n", "h1", whln],
+    ["(\n)\\*{3,}\n", "hr", whln],
+    ["(\n)_{3,}\n", "hr", whln],
+    ["([^\\\\])\\*\\*(.*?[^\\\\])\\*\\*", "b", inln],
+    ["([^\\\\])\\*(.*?[^\\\\])\\*", "i", inln],
+    ["([^\\\\])__(.*?[^\\\\])__", "b", inln],
+    ["([^\\\\])_(.*?[^\\\\])_", "i", inln],
+    ["([^\\\\])~~(.*?[^\\\\])~~", "s", inln],
+    ["(\n)(.*?)\n=+?\n", "h1", whln],
+    ["(\n)(.*?)\n-+?\n", "h2", whln]]);
 }
