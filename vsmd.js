fde0707d6364e053f10c68a0f3657c215769a355
Mateusz Ezechiel Górka <admin@yehezkel.cba.pl>
2018-07-28 03:40:36 +0200

    refactoring of that regexp list

--- vsmd.js
+++ vsmd.js
@@ -1,47 +1,47 @@
 function markdown(code) {
-  const inln = "";    /* inline rule */
-  const whln = "\n";  /* whole line rule*/
-
   /*
-    p0  group which will always be printed
-    p1  ACTUAL TEXT
-    p2  a/img src/href
-    p3  img title
+    ord    ordinary text, always printed
+    value  formatted text
+    src    src/href
+    title  img title
   */
 
+  const getStr =
+    (obj) => Object.prototype.toString.call(obj) === '[object String]' ? obj : "";
+
   const makeTag =
-    (tag, mode, p0, value, p2, p3) => p0 + (tag == "img_with_title"
-      ? "<img alt=\"" + value + "\" src=\"" + p2 + "\" title=\"" + p3 + "\" />"
+    (tag, ord, value, src, title) => ord + (tag == "img_with_title"
+      ? "<img alt=\"" + value + "\" src=\"" + src + "\" title=\"" + title + "\" />"
       : tag == "img_without_title"
-        ? "<img alt=\"" + value + "\" src=\"" + p2 + "\" />"
+        ? "<img alt=\"" + value + "\" src=\"" + src + "\" />"
         : tag == "a"
-          ? "<a href=\"" + p2 + "\">" + value + "</a>"
-          : typeof value == "string"
-            ? "<" + tag + ">" + value + "</" + tag + ">"
-            : "<" + tag + " />") + mode;
+          ? "<a href=\"" + src + "\">" + value + "</a>"
+          : getStr(value)
+            ? "<" + tag + ">" + value + "</" + tag + ">" + getStr(src)
+            : "<" + tag + " />"+ getStr(src));
 
   const applyRules = 
     (str, arr) =>
       arr.reduce((prev, cur) =>
         prev.replace(new RegExp(cur[0], "g"), /* str.replace(rule, tag) */
-                     (a, p0, p1, p2, p3) => makeTag(cur[1], cur[2], p0, p1, p2, p3)), str);
+                     (a, p0, p1, p2, p3) => makeTag(cur[1], p0, p1, p2, p3)), str);
   
-  code = "\n" + code;
+  code = "\n" + code + "\n";
 
   return applyRules(code, [
-    ["(\n)[ \t]*######[ \t]*(.*?)\n", "h6", whln], 
-    ["(\n)[ \t]*#####[ \t]*(.*?)\n", "h5", whln],
-    ["(\n)[ \t]*####[ \t]*(.*?)\n", "h4", whln],
-    ["(\n)[ \t]*###[ \t]*(.*?)\n", "h3", whln],
-    ["(\n)[ \t]*##[ \t]*(.*?)\n", "h2", whln],
-    ["(\n)[ \t]*#[ \t]*(.*?)\n", "h1", whln],
-    ["(\n)\\*{3,}\n", "hr", whln],
-    ["(\n)_{3,}\n", "hr", whln],
-    ["([^\\\\])\\*\\*(.*?[^\\\\])\\*\\*", "b", inln],
-    ["([^\\\\])\\*(.*?[^\\\\])\\*", "i", inln],
-    ["([^\\\\])__(.*?[^\\\\])__", "b", inln],
-    ["([^\\\\])_(.*?[^\\\\])_", "i", inln],
-    ["([^\\\\])~~(.*?[^\\\\])~~", "s", inln],
-    ["(\n)(.*?)\n=+?\n", "h1", whln],
-    ["(\n)(.*?)\n-+?\n", "h2", whln]]);
+    ["(\n)[ \t]*######[ \t]*(.*)", "h6"], 
+    ["(\n)[ \t]*#####[ \t]*(.*)", "h5"],
+    ["(\n)[ \t]*####[ \t]*(.*)", "h4"],
+    ["(\n)[ \t]*###[ \t]*(.*)", "h3"],
+    ["(\n)[ \t]*##[ \t]*(.*)", "h2"],
+    ["(\n)[ \t]*#[ \t]*(.*)", "h1"],
+    ["(\n)\\*{3,}()", "hr"],
+    ["(\n)_{3,}()", "hr"],
+    ["([^\\\\])\\*\\*(.*?[^\\\\])\\*\\*", "b"],
+    ["([^\\\\])\\*(.*?[^\\\\])\\*", "i"],
+    ["([^\\\\])__(.*?[^\\\\])__", "b"],
+    ["([^\\\\])_(.*?[^\\\\])_", "i"],
+    ["([^\\\\])~~(.*?[^\\\\])~~", "s"],
+    ["(\n)(.*?)\n[ \t]*=+[ \t]*(\n)", "h1"],
+    ["(\n)(.*?)\n[ \t]*-+[ \t]*(\n)", "h2"]]).slice(1, -1);
 }
